00016d88c02875af5c78b9ad0d54d6a52ede4c26
Pavan <vpavankumar1999@gmail.com>
2024-02-06 22:10:54 +0530

    Initial commit.

---
+++ WebAppSchoolDb/WebAppSchoolDb/Models/Class.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebAppSchoolDb.Models
+{
+    public partial class Class
+    {
+        public int Class1 { get; set; }
+    }
+}
---
+++ WebAppSchoolDb/WebAppSchoolDb/Models/Subject.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebAppSchoolDb.Models
+{
+    public partial class Subject
+    {
+        public int SubId { get; set; }
+        public string? SubName { get; set; }
+    }
+}
